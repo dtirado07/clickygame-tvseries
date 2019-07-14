d1fcf406eb4af775acb6997dabf811d5fc1eb120
dtirado <dtirado80@msn.com>
2019-07-13 23:20:19 -0500

    Updates

---
+++ precache-manifest.1f3a16c4ceb21b744c2a48c74c2ac6ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17880141f6b6f4a02863f6be87fa322a",
+    "url": "/clickygame-tvseries/index.html"
+  },
+  {
+    "revision": "2936a09fb990f0fb6159",
+    "url": "/clickygame-tvseries/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clickygame-tvseries/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2936a09fb990f0fb6159",
+    "url": "/clickygame-tvseries/static/js/main.a81c3f39.chunk.js"
+  },
+  {
+    "revision": "5152605ddf2cf4a598cf",
+    "url": "/clickygame-tvseries/static/js/runtime~main.c55e592d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickygame-tvseries/static/media/logo.5d5d9eef.svg"
+  }
+]);
